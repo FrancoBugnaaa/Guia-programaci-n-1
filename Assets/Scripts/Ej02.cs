04671095ebdc615ec5091d2cfcaa2a0ce8da992d
FrancoBugnaaa <soyderiver777@gmail.com>
2022-03-29 15:19:30 -0300

    Ej 08
    Completed

--- Assets/Scripts/Ej02.cs
+++ Assets/Scripts/Ej02.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Ej02 : MonoBehaviour
 {
     // Start is called before the first frame update
+    //2. Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,
+//respectivamente, realice la operación correspondiente y muestre el resultado en pantalla:
+//a.Suma
+//b.Producto
     void Start()
     {
         
